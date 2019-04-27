87e602e6244f4042d2d64b7504259483589d4d52
Angelo Yan <angeloyanemail@gmail.com>
2019-04-27 19:23:40 +1000

    added get request

--- public/js/create.js
+++ public/js/create.js
@@ -1,6 +1,17 @@
 function submitTopic() {
     let topic = document.getElementById("search-box").value;
     console.log(topic)
+
+    // send topic to backend
+    const Http = new XMLHttpRequest();
+    const url = 'https://auto-quiz-backend.herokuapp.com/' + topic;
+    Http.open("GET", url);
+    Http.send();
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
+    }
+
+    // generate room key
     function makeid(length) {
         var result = '';
         var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
