0bd02a1b088a17ce968adefdf4a966777e46d572
Angelo Yan <angeloyanemail@gmail.com>
2019-04-27 18:58:45 +1000

    added random number generator

--- public/js/create.js
+++ public/js/create.js
@@ -1,4 +1,15 @@
 function submitTopic() {
     let topic = document.getElementById("search-box").value;
     console.log(topic)
+    function makeid(length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+
+    console.log(makeid(4));
 }
