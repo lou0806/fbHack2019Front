d38c752dc6d60baaa3e9910ecea870c061746ab4
Angelo Yan <angeloyanemail@gmail.com>
2019-04-27 17:38:50 +1000

    changed file structure

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use('/views', express.static('views'));
+
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 io.on('connection', function (socket) {
