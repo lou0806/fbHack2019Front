15ba55f50db81687304dd4bb8d6235bc0aafad2f
lou0806 <luigiye598@gmail.com>
2019-04-27 21:53:28 +1000

    merged question.html and join.html, laid out join.html <div> tags, working on basic .js

---
+++ public/js/join.js
@@ -0,0 +1,20 @@
+//TODO: Complete
+//sends user info to server
+var users=["dude1","dude2","dude3"]
+
+function submitUserInfo() {
+    let userName = document.getElementById("sign-in").value;
+    console.log(userName)
+
+    //send userName to server TODO
+
+}
+
+//starts the Questions on the prompt of Creator clicking Start.
+//TODO: make this true when Creator clicks Start
+function startQuestions() {
+    //when server sends signal to start, hide div "sign-in", display div "question"
+}
+
+function displayEndStats() {
+}
